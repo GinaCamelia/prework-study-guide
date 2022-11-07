46e391c14d803c5d29e75985f9832d93aa0fe03f
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-11-07 12:55:32 +0000

    encapsulated previous code in functions for better reusability

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 let topics = ['HTML', 'CSS', 'JavaScript', 'Git'];
-for (x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
+let randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+function listTopics() {
+  for (x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+    }
 }
-*/
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    } 
+}
+console.log('Which topic should we study first?');
+selectTopic();
